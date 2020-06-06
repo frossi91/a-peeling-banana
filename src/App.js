8f7f44aba490ea66496dc8eb6094aee6f20e7598
Frank Rossi <frossi@apprisemobile.com>
2020-06-06 14:27:59 -0400

    implemented artifical server delay, loading indiciator, error messaging

--- src/App.js
+++ src/App.js
@@ -1,96 +1,135 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Container, Row, Col } from 'react-grid-system';
 import includes from 'lodash/includes'
+import debounce from 'lodash/debounce'
 import logo from './assets/daily-harvest-logo.jpg'
 import resolveIngredientSearch, { getIngredientById } from './resolvers/ingredient-search'
 import './App.css'
 
 const DEFAULT_INGREDIENT_SEARCH = 'Organic Banana'
 const DEFAULT_PLACEHOLDER_TEXT = 'Hungry for ...'
+const DEFAULT_SIMULATED_NETWORK_DELAY = 250
+const DEFAULT_SEARCH_RESULTS = resolveIngredientSearch(DEFAULT_INGREDIENT_SEARCH)
 
-function ProductList ({ products, searchValue }) {
+function ProductList ({ products, searchValue, loading }) {
+
   const buildBoldedIngredient = (ingredient, searchValue) => {
+    // trimming this just in case there is bad data in the DB
     let ingredientName = ingredient.name.trim()
     // find the index of the search value, this way we can bold the partial results
     const startIndex = ingredientName.toLowerCase().indexOf(searchValue.trim().toLowerCase())
     const endIndex = startIndex + searchValue.length
+
     const prefixedText = ingredientName.substring(0, startIndex)
     const boldedText = ingredientName.substring(startIndex, endIndex)
     const postText = ingredientName.substring(endIndex, ingredientName.length)
     return (
-      <li style={{textAlign: 'left'}}>
+      <li key={`ingredient-${ingredient.id}`} className="Left">
         <span>{prefixedText}<b>{boldedText}</b>{postText}</span>
       </li>
     )
   }
+
   const buildIngredientList = (product) => {
     return product.ingredient_ids.map(ingredientId => {
       const ingredient = getIngredientById(ingredientId)
       if (includes(product.matchingIngredientsIds, ingredientId)) {
         return buildBoldedIngredient(ingredient, searchValue)
-        // return <li style={{textAlign: 'left'}}>{ingredient.name}</li>
-      } else return <li style={{textAlign: 'left'}}>{ingredient.name}</li>
+      } else return <li key={ingredientId} className="Left">{ingredient.name}</li>
     })
   }
-  const productNames = products.map(product => {
-    const ingredientList = buildIngredientList(product)
+
+  const productListItems = products.map(product => {
     return (
-      <li>
-        <div style={{textAlign: 'left', fontWeight: 'bold'}}> 
-          <span>{product.name}</span>
-        </div>
+      <li key={`product-${product.id}`}>
+        <span>{product.name}</span>
         <ul>
-          {ingredientList}
+          {buildIngredientList(product)}
         </ul>
       </li>
     )
   })
+
   return (
     <ul>
-      {productNames}
+      {productListItems}
     </ul>
   )
 }
 
 function IngredientSearch () {
+  const [loading, setLoading] = useState(false)
   const [searchValue, setSearchValue] = useState(DEFAULT_INGREDIENT_SEARCH)
-  const [searchResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState(DEFAULT_SEARCH_RESULTS)
   
-  useEffect(() => {
-    const _searchResults = resolveIngredientSearch(searchValue)
+  const delayedSearch = useRef(debounce(async (_searchValue) => {
+    // add an artificial delay of 500 ms to simulate a network request
+    await new Promise(resolve => setTimeout(resolve, DEFAULT_SIMULATED_NETWORK_DELAY))
+    const _searchResults = resolveIngredientSearch(_searchValue)
     setSearchResults(_searchResults)
-  }, [searchValue])
+    setLoading(false)
+  }, 300, {trailing: true})).current
 
   const handleInputChange = (e) => {
+    setLoading(true) // setting loading to true here to account for the debounce time as well as simulated network time
     setSearchValue(e.target.value)
+    delayedSearch(e.target.value.trim())
   }
 
+  const getLoadingOrNoResults = () => {
+    if (loading) return <div className="Loader"></div>
+    if (searchValue && searchResults.length === 0) {
+      return (
+        <Col>
+          <div className="SorryMessage">
+            <span>{`Sorry! Looks like we are fresh out of`}</span>
+            <br></br>
+            <span><i>{`"${searchValue}"`}</i></span>
+          </div>
+        </Col>
+      )
+    }
+  }
+
+  // still need to debounce
   return (
-    <div>
+    <React.Fragment>
       <Row>
-        <input className="Search-input" placeholder={DEFAULT_PLACEHOLDER_TEXT} value={searchValue} type='search' onChange={handleInputChange}></input>
+        <input className="SearchInput" placeholder={DEFAULT_PLACEHOLDER_TEXT} value={searchValue} type='search' onChange={handleInputChange}></input>
       </Row>
       <Row>
-        <ProductList products={searchResults} searchValue={searchValue} />
+        { 
+          loading || searchResults.length === 0 ? getLoadingOrNoResults() : <ProductList products={searchResults} searchValue={searchValue}/>
+        }
       </Row>
-    </div>
+    </React.Fragment>
   )
 }
 
-function App() {
+/**
+   * App is built off a simple grid system.  Usually this is provided through an entire UI library suite such as Bootstrap
+   * Bringing in something like Bootstrap would give me a lot of stuff I do not need, so I found a nice grid system library to use instead
+   * https://github.com/sealninja/react-grid-system
+   * 
+   * App renders The Daily Harvest logo and a React Component that handles the search and it's results
+   * 
+   */
+function App() { 
   return (
-    <Container fluid style={{minWidth: '320px'}}>
-      <Row>
-        <Col xs={1} sm={3}></Col>
-        <Col xs={10} sm={6}>
+    <Container className='AppContainer' fluid>
+      <Row justify='center'>
+        {/* gutter */}
+        <Col xs={1} sm={3} lg={3}></Col>
+        <Col xs={10} sm={6} lg={4}>
           <div>
             <Row>
-              <img style={{width: '100%'}} src={logo} className="App-logo" alt="logo" />
+              <img className="AppLogo" src={logo} alt="logo" />
             </Row>
             <IngredientSearch />
           </div>
         </Col>
-        <Col xs={1} sm={6}></Col>
+        {/* gutter */}
+        <Col xs={1} sm={3} lg={4}></Col>
       </Row>
     </Container>
   );
--- src/resolvers/ingredient-search.js
+++ src/resolvers/ingredient-search.js
@@ -1,8 +1,16 @@
 /**
  * Putting search logic in it's own file.  Ideally in a production environment, we would aim to do this search serverside with full text search support.
  */
+
+// data imports
 import { products } from '../data/products.json'
 import { ingredients } from '../data/ingredients.json'
+
+/**
+ * lodash imports
+ * I'm a big fan of this library.  I think I've probably included it in every node project I've built.
+ * A lot of this functionality is now baseline with ES6+, but lodash aims to optimize commonly used intertee functions
+ */
 import find from 'lodash/find'
 import reduce from 'lodash/reduce'
 import includes from 'lodash/includes'
@@ -10,38 +18,66 @@ import filter from 'lodash/filter'
 import intersection from 'lodash/intersection'
 
 /**
- * I decided to make the search work off an includes rather than a strict equality.
+ * resolveIngredientSearch
+ * @param {String} searchTerm 
+ * 
+ * I decided to make the search function off an "includes" rather than strict equality.
  * This way, we can support partial matches.
- * If a use searches "Organic", they will see all results with organic ingredients.
+ * If a user searches "Organic", they will see all results that contain "organic" anywhere in the ingredient name.
  */
-
 function resolveIngredientSearch(searchTerm) {
-  // sanitize the input
-  searchTerm = searchTerm.trim().toLowerCase()
+  /**
+   * sanitize the input by trimming away any trailing spaces
+   * lowercase the input for text comparison so our search is case-insensitive
+   * technically this is a second trim() since the component already trimed the value on the component side...but wanted to keep this here to simulate a server environment
+   */
+  const _searchTerm = searchTerm.trim().toLowerCase()
   
-  // early return for empty string case
-  if (!searchTerm) return []
+  // Early out here for wher the user clears out the input
+  if (!_searchTerm) return []
+  
   /**
    * First we need to find the IDs of the ingredients that match the serach term
+   * Here we reduce the array of ingredient objects down to an array of ingredient IDs that match the search value
    */
-  const searchIngredientIds = reduce(ingredients, (result, ingredient, key) => {
-    if (includes(ingredient.name.toLowerCase(), searchTerm)) result.push(ingredient.id)
+  const searchIngredientIds = reduce(ingredients, (result, ingredient) => {
+    /**
+     * here im checking if the ingredient name includes our search term, remember to lowercase for case-insensitivity
+     * if the the includes passes, the ingredient ID is pushed on to the resulting array
+     */ 
+    if (includes(ingredient.name.toLowerCase(), _searchTerm)) result.push(ingredient.id)
     return result
   }, [])
 
-  return filter(products, (product) => {
-    /**
-     * If the intersection of productIngredientIds and searchResultIngredientIds is greater than 0
-     * it means the product contains at least 1 matching ingredient, so return it in the result.
-     */
+  /**
+   * now that we have an array of ingredient IDs based off the users search,
+   * the last thing we need to do is filter the product data for those that included any of the IDs in the searchIngredientIds array
+   * if the intersection of product.ingredient_ids and searchResultIngredientIds is greater than 0,
+   * it means the product contains at least 1 matching ingredient, so return it in the result.
+   */
+  const filtered = filter(products, product => {
+    // create the intersecting array
     const matchingIngredientsIds = intersection(product.ingredient_ids, searchIngredientIds)
+
+    /**
+     * saving off new array on product that represents which ingredients matched the users search
+     * I will later leverage this array to bolden the matching serch term in the UI
+     */ 
     product.matchingIngredientsIds = matchingIngredientsIds
+
+    // return boolean to indicate if product should be included in filter
     return matchingIngredientsIds.length > 0
   })
-}
 
+  return filtered
+}
+export default resolveIngredientSearch
+
+/**
+ * getIngredientById
+ * @param {Int} ingredientId 
+ * get the ingredient object by its ID
+ */
 export const getIngredientById = (ingredientId) => {
   return find(ingredients, (ingredient) => ingredient.id === ingredientId)
 }
-
-export default resolveIngredientSearch
