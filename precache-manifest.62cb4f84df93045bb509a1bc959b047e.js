e7dac9db4935aa8908a15c389bd8c56405c930b0
Frank Rossi <frossi@apprisemobile.com>
2020-06-06 06:58:45 -0400

    Updates

---
+++ precache-manifest.62cb4f84df93045bb509a1bc959b047e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80adc42528fad406b4fed7346387f12",
+    "url": "/a-peeling-banana/index.html"
+  },
+  {
+    "revision": "33fdddfd6f235ff0ebf5",
+    "url": "/a-peeling-banana/static/css/main.ed352093.chunk.css"
+  },
+  {
+    "revision": "d9517372688afe2bc8e6",
+    "url": "/a-peeling-banana/static/js/2.179f82ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a-peeling-banana/static/js/2.179f82ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33fdddfd6f235ff0ebf5",
+    "url": "/a-peeling-banana/static/js/main.0ce28bb7.chunk.js"
+  },
+  {
+    "revision": "a39a94c9c22fc15ff51f",
+    "url": "/a-peeling-banana/static/js/runtime-main.e39cef41.js"
+  },
+  {
+    "revision": "68c0e9f0fe48fbadecdf3166633f69f8",
+    "url": "/a-peeling-banana/static/media/daily-harvest-logo.68c0e9f0.jpg"
+  }
+]);
